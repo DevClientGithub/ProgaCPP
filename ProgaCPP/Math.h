39715669c88cc43afe436b4144b16a45b01abfc1
KelzerZ <kelzerkelzer350@gmail.com>
2023-07-18 19:30:30 +0300

    Обновление Math
    Во-первых я поменял класс на namespace.
    
    Во-вторых я убрал ненужные методы add, sub.
    
    В-третьих я изменил метод dupeData так, чтоб он принимал не только тип int. Я добавил generic тип.

--- ProgaCPP/Math.h
+++ ProgaCPP/Math.h
@@ -3,37 +3,31 @@
 #include <cstdlib>
 #include <ctime>
 
-class Math {
-public:
-    static int add(int a, int b) {
-        return a + b;
-    }
+namespace Math {
+	int random(int min, int max) {
+		static bool isSeeded = false;
+		if (!isSeeded) {
+			srand(static_cast<unsigned int>(time(0)));
+			isSeeded = true;
+		}
+		return rand() % (max - min + 1) + min;
+	}
 
-    static int sub(int a, int b) {
-        return a - b;
-    }
+	int antirandom(int a, int b) {
+		int randomValue = random(a, b);
+		return a + b - randomValue;
+	}
 
-    static int random(int min, int max) {
-        static bool isSeeded = false;
-        if (!isSeeded) {
-            srand(static_cast<unsigned int>(time(0)));
-            isSeeded = true;
-        }
-        return rand() % (max - min + 1) + min;
-    }
+	template<typename T>
+	void dupeData(T*& variable, const size_t& size) {
+		T* newVariable = new int[size * 2];
 
-    static int antirandom(int a, int b) {
-        int randomValue = random(a, b);
-        return a + b - randomValue;
-    }
+		for (int i = 0; i < size; i++) {
+			newVariable[i] = variable[i];
+		}
 
-    static void dupeData(int*& variable, int size) {
-        int* newVariable = new int[size * 2];
-        for (int i = 0; i < size; i++) {
-            newVariable[i] = variable[i];
-        }
-        delete[] variable;
-        variable = newVariable;
-    }
+		delete[] variable;
+		variable = newVariable;
+	}
 
 };
