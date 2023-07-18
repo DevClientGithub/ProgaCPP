bc6f398bdece94ca1290ce930ccc6d03ec32c45a
KelzerZ <kelzerkelzer350@gmail.com>
2023-07-18 16:57:31 +0300

    Сделал начальную реализацию класса List.
    Добавил конструкторы.
    Добавил метод length.
    Реализовал метод push, он добавляет новый элемент в конец листа.

--- ProgaCPP/List.h
+++ ProgaCPP/List.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <iostream>
+
+using namespace std;
+
 template <typename T>
 class List {
 private:
@@ -17,5 +21,35 @@ private:
 	size_t size;
 
 public:
+	List() {
+		this->head = nullptr;
+		this->size = 0;
+	}
 
+	List(const initializer_list<T>& items) {
+		for (const T& item : items) {
+			this->push(item);
+		}
+	}
+
+	size_t length() {
+		return this->size;
+	}
+
+	void push(const T& item) {
+		if (this->head == nullptr) {
+			this->head = new Node(item);
+		}
+		else {
+			Node* current = this->head;
+
+			while (current->next != nullptr) {
+				current = current->next;
+			}
+
+			current->next = new Node(item);
+		}
+
+		this->size++;
+	}
 };
