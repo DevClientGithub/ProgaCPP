3f0e1059b0b01af3926124233201386cdb621c26
DevClientGithub <devclientgithub@gmail.com>
2023-07-19 22:27:02 +0300

    Mega Update User Class Add
    Обновил Main.cpp чтоб ты знал как юзать эту хуйню, и добавил класс

--- ProgaCPP/Main.cpp
+++ ProgaCPP/Main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "User.h"
 
 int main() {
-	int sus = 5;
-	std::cout << sus;
+	Database TestBase;
+
+	TestBase.addUser(User("Gandon", 0));
+	TestBase.addUser(User("Pizda", 1));
+	TestBase.addUser(User("Eblan", 2));
+	TestBase.addUser(User("Stupid", 3));
+	TestBase.addUser(User("Wtf", 4));
+
+	User* user = TestBase.findUserByName("Stupid");
+	user = TestBase.findUserByName("Wtf");
+	if (user) {
+		std::cout << "Found User: " << user->getName() << ", Id: " << user->getId() << std::endl;
+	}
+	else {
+		std::cout << "User not found" << std::endl;
+	}
+
+	return 0;
+
+	// int sus = 5;
+	// std::cout << sus;
 }
