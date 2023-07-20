8259d354305db23325f15ca713ecdda1e8f85f1a
KelzerZ <kelzerkelzer350@gmail.com>
2023-07-20 18:48:36 +0300

    Чуток переделал твой код под this-> Так красивее

--- ProgaCPP/String.h
+++ ProgaCPP/String.h
@@ -8,31 +8,34 @@ private:
 	size_t size; // Длина строки
 public:
     String(const char* str) {
-        size = strlen(str);
-        data = new char[size + 1];
-        for (size_t i = 0; i < size; i++) {
-            data[i] = str[i];
+        this->size = strlen(str);
+
+        this->data = new char[this->size + 1];
+
+        for (size_t i = 0; i < this->size; i++) {
+            this->data[i] = str[i];
         }
-        data[size] = '\0';
+
+        this->data[this->size] = '\0';
     }
 
     ~String() {
-        delete[] data;
+        delete[] this->data;
     }
     // Узнать длину строки
-    size_t length() const {
-        return size;
+    size_t length() const noexcept {
+        return this->size;
     }
     // Сделать всю строку маленькой (Через маленькие символы)
     void toLowerCase() {
         for (int i = 0; i < size; i++) {
-            data[i] = std::tolower(static_cast<unsigned char>(data[i]));
+            this->data[i] = std::tolower(static_cast<unsigned char>(this->data[i]));
         }
     }
     // Сделать всю строку большой (Через большие символы)
     void toUpperCase() {
         for (int i = 0; i < size; i++) {
-            data[i] = std::toupper(static_cast<unsigned char>(data[i]));
+            this->data[i] = std::toupper(static_cast<unsigned char>(this->data[i]));
         }
     }
     friend std::ostream& operator<<(std::ostream& os, const String& str) {
