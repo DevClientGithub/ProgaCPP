2326013a656639e0cdea9efeb36d896fb1b0f70b
KelzerZ <kelzerkelzer350@gmail.com>
2023-08-26 19:04:45 +0300

    Добавил includes.h

---
+++ ProgaCPP/includes.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <array>
+#include <string>
+
+#include "Array.h"
+#include "List.h"
+#include "Math.h"
+#include "Ram.h"
+#include "String.h"
+#include "Vector.h"
