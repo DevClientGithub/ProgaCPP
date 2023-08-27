6a4877803eab4c301ac521e0e3ca11a78df49d98
KelzerZ <kelzerkelzer350@gmail.com>
2023-08-28 01:11:28 +0300

    Убрал к хуям сортировку, т.к. она не подвергается  оптимизации. Точнее твой код* Нужно сделать сортировку про для массива

--- ProgaCPP/Array.h
+++ ProgaCPP/Array.h
@@ -40,52 +40,6 @@ public:
 		return size;
 	}
 
-	// Быстрая сортировка 1
-	Array quickSortRecursive(T arr[size], int low, int high) {
-		if (low < high) {
-			T pivot = arr[(low + high) / 2];
-			int i = low;
-			int j = high;
-
-			while (i <= j) {
-				while (arr[i] < pivot)
-					++i;
-				while (arr[j] > pivot)
-					--j;
-				if (i <= j) {
-					std::swap(arr[i], arr[j]);
-					++i;
-					--j;
-				}
-			}
-
-			if (low < j)
-				quickSortRecursive(arr, low, j);
-			if (i < high)
-				quickSortRecursive(arr, i, high);
-		}
-
-		Array result;
-		std::copy(arr, arr + size, result.items);
-		return result;
-	}
-
-	Array quickSort(T arr[size]) {
-		Array result = quickSortRecursive(arr, 0, size - 1);
-		return result;
-	}
-
-	// Пузырьковая сортировка
-	void bubbleSort() {
-		for (size_t i = 0; i < size - 1; ++i) {
-			for (size_t j = 0; j < size - i - 1; ++j) {
-				if (this->items[j] > this->items[j + 1]) {
-					swap(this->items[j], this->items[j + 1]);
-				}
-			}
-		}
-	}
-
 	// Заполнить массив стандартными значениями
 	void clear() {
 		for (size_t i = 0; i < size; i++) {
