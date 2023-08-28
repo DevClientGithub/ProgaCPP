e4eaae1e3025782ae0e70402b06e1dc7d752c397
KelzerZ <kelzerkelzer350@gmail.com>
2023-08-28 19:08:18 +0300

    Изменил Math.h, сделал его более адекватным.

--- ProgaCPP/Math.h
+++ ProgaCPP/Math.h
@@ -1,57 +1,45 @@
 #pragma once
 
-#include <cstdlib>
-#include <ctime>
-
-constexpr double PI_D = 3.141592653589793; // constexpr как и define не тратят байты в памяти
-constexpr float PI_F = 3.1415927f;
-constexpr double E_D = 2.718281828459045;
-constexpr float E_F = 2.7182817f;
-constexpr double GR_D = 1.618033988749895;
-constexpr float GR_F = 1.6180339f;
-constexpr double TO_RADIAN = 0.017453292519943295;
-
 namespace Math {
-	int random(const int& min, const int& max) {
-		bool isSeeded = false;
+	constexpr double PI_D = 3.141592653589793;
+	constexpr float PI_F = 3.1415927f;
 
-		if (!isSeeded) {
-			srand(static_cast<unsigned int>(time(0)));
-			isSeeded = true;
-		}
+	constexpr double E_D = 2.718281828459045;
+	constexpr float E_F = 2.7182817f;
 
-		return rand() % (max - min + 1) + min;
-	}
+	constexpr double GR_D = 1.618033988749895;
+	constexpr float GR_F = 1.6180339f;
 
-	int antirandom(const int& a, const int& b) {
-		int randomValue = random(a, b);
-		return a + b - randomValue;
-	}
-
-	template<typename T>
-	void dupeData(T*& variable, const size_t& size) {
-		T* newVariable = new int[size * 2];
-
-		for (int i = 0; i < size; i++) {
-			newVariable[i] = variable[i];
-		}
-
-		delete[] variable;
-		variable = newVariable;
-	}
+	constexpr double TO_RADIAN_D = 0.017453292519943295;
+	constexpr float TO_RADIAN_F = 0.017453292f;
 
 	double sin(const double& angle, const bool& isRadian = true) {
-		const double res = ::sin(isRadian ? angle : angle * TO_RADIAN);
+		const double res = ::sin(isRadian ? angle : angle * TO_RADIAN_D);
+		return res;
+	}
+
+	float sin(const float& angle, const bool& isRadian = true) {
+		const float res = ::sin(isRadian ? angle : angle * TO_RADIAN_F);
 		return res;
 	}
 
 	double cos(const double& angle, const bool& isRadian = true) {
-		const double res = ::cos(isRadian ? angle : angle * TO_RADIAN);
+		const double res = ::cos(isRadian ? angle : angle * TO_RADIAN_D);
+		return res;
+	}
+
+	float cos(const float& angle, const bool& isRadian = true) {
+		const double res = ::cos(isRadian ? angle : angle * TO_RADIAN_F);
 		return res;
 	}
 
 	double tan(const double& angle, const bool& isRadian = true) {
-		const double res = ::tan(isRadian ? angle : angle * TO_RADIAN);
+		const double res = ::tan(isRadian ? angle : angle * TO_RADIAN_D);
+		return res;
+	}
+
+	float tan(const float& angle, const bool& isRadian = true) {
+		const float res = ::tan(isRadian ? angle : angle * TO_RADIAN_F);
 		return res;
 	}
 
