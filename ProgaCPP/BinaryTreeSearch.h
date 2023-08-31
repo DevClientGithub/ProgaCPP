8f55ca01d16d4f7dc0985e034d223747009ca6eb
KelzerZ <kelzerkelzer350@gmail.com>
2023-08-31 05:57:43 +0300

    Созданите бинарного дерева.

--- ProgaCPP/BinaryTreeSearch.h
+++ ProgaCPP/BinaryTreeSearch.h
@@ -1,3 +1,31 @@
 #pragma once
 
-class BinaryTreeSearch {};
+typedef long long int Int64;
+
+template <typename T>
+class BinaryTreeSearch {
+private:
+	class Node {
+		Int64 key;
+		T value;
+
+		Node* right = nullptr;
+		Node* left = nullptr;
+
+		Node(const Int64& key, const T& value) : key(key), value(value) {}
+	};
+
+public:
+	Node* root = nullptr;
+
+	void add(const Int64& key, const T& value, const Node*& node = this->root) {
+		if (node == nullptr) {
+			node = new Node(key, value);
+			return;
+		}
+
+		if (key > node->key) {
+
+		}
+	}
+};
