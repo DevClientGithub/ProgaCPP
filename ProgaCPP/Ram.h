dee87174b08a711c40ad8d3cfd14383c6158b858
DevClientGithub <devclientgithub@gmail.com>
2023-09-03 01:34:18 +0300

    Add GetComputerName() and GetUserName()
    Если что, мы скорее всего это даже в Master пихать не будем, так как тут я буду тестить прям жесть.

--- ProgaCPP/Ram.h
+++ ProgaCPP/Ram.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <windows.h>
+#include <string>
 
 class Ram {
 private:
     SIZE_T totalPhysicalMemory;
     MEMORYSTATUSEX memoryStatus;
+    std::string computerName; // Имя компьютера
+    std::string userName; // Имя пользователя
 
 public:
     // Получает общий объем памяти и другие свойства системной памяти
@@ -13,6 +16,13 @@ public:
         memoryStatus.dwLength = sizeof(memoryStatus);
         GlobalMemoryStatusEx(&memoryStatus);
         totalPhysicalMemory = memoryStatus.ullTotalPhys;
+        char buffer[MAX_COMPUTERNAME_LENGTH + 1];
+        DWORD size = sizeof(buffer);
+        GetComputerNameA(buffer, &size);
+        computerName = buffer;
+        size = sizeof(buffer);
+        GetUserNameA(buffer, &size);
+        userName = buffer;
     }
 
     // Узнать сколько занято памяти в процентах
@@ -42,5 +52,15 @@ public:
     template<typename T>
     static void showSize(const T& obj) {
         std::cout << "Size of the object: " << sizeof(obj) << " bytes." << std::endl;
+    }
+
+    // Узнать имя компьютера
+    std::string GetComputerName() {
+        return computerName;
+    }
+
+    // Узнать имя пользователя
+    std::string GetUserName() {
+        return userName;
     }
 };
