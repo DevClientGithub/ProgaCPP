b0d3a3ede18aa91ba84fa92be644f6e9a839658a
kelzerz <kelzerkelzer350@gmail.com>
2023-10-04 22:23:33 +0300

    V1 Бинарного Дерева
    Реализовал основные методы(Добавления узла, поиска узла и очистки дерева)
    
    Осталось реализовать методы getMax, getMin, clearNode и т.д.
    
    Однако, я смог сделать базу, ура!!!
    Вроде как ошибок нету, да и на практике не получал

--- ProgaCPP/BinaryTreeSearch.h
+++ ProgaCPP/BinaryTreeSearch.h
@@ -1,16 +1,77 @@
 #pragma once
 
+template <typename T>
 class BinaryTreeSearch {
 private:
 	typedef long long int Int64;
 
 	class Node {
-	public:
-		Int64 value;
-
-		Node* right = nullptr;
+	private:
 		Node* left = nullptr;
+		Node* right = nullptr;
+	public:
+		const Int64 key;
+		T value;
 
-		Node(const Int64& value) : value(value) {}
+		friend class BinaryTreeSearch;
+
+	Node(const Int64& key, const T& value): key(key), value(value) {}
 	};
+
+	Node* root = nullptr;
+
+public:
+	~BinaryTreeSearch() {
+		this->clear();
+	}
+
+	Node* add(const Int64& key, const T& value, Node*& node) {
+		if (node == nullptr) {
+			node = new Node(key, value);
+		}
+		else if (key < node->key) {
+			this->add(key, value, node->left);
+		}
+		else if(key > node->key) {
+			this->add(key, value, node->right);
+		}
+
+		return node;
+	}
+
+	Node* add(const Int64& key, const T& value) {
+		return this->add(key, value, this->root);
+	}
+
+	Node* search(const Int64& key, Node* node) {
+		if (node->key == key) {
+			return node;
+		}
+
+		if (key < node->key) {
+			return this->search(key, node->left);
+		}
+
+		if (key > node->key) {
+			return this->search(key, node->right);
+		}
+
+		return node;
+	}
+
+	Node* search(const Int64& key) {
+		return this->search(key, this->root);
+	}
+
+	void clear(Node* node) {
+		if (node != nullptr) {
+			this->clear(node->left);
+			this->clear(node->right);
+			delete node;
+		}
+	}
+
+	void clear() {
+		this->clear(this->root);
+	}
 };
--- ProgaCPP/Main.cpp
+++ ProgaCPP/Main.cpp
@@ -3,4 +3,6 @@
 int main() {
     int sus = 5;
     std::cout << sus;
+
+    return 0;
 }
